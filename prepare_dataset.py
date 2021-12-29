af1c40b2427af26a16dd67883c6d6ffbe7a9b253
Cheng Zhang <zhangchengx@gmail.com>
2021-12-29 18:25:42 -0500

    Update prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -12,23 +12,15 @@ import os
 
 def load_squad_dataset(dataset):
     df_dataset = pd.DataFrame(columns=['context', 'question', 'answer'])
-    df_dataset_long = pd.DataFrame(columns=['context', 'question', 'answer'])
-    num_of_long_answer = 0
-    num_of_short_answer = 0
+    num_of_answer = 0
     for index, value in tqdm(enumerate(dataset)):
         context = value['context']
         question = value['question']
         answer = value['answers']['text'][0]
         number_of_words = len(answer.split())
-        if number_of_words >= 8:
-            df_dataset_long.loc[num_of_long_answer] = [context] + [question] + [answer]
-            num_of_long_answer = num_of_long_answer + 1
-        else:
-            df_dataset.loc[num_of_short_answer] = [context] + [question] + [answer]
-            num_of_short_answer = num_of_short_answer + 1
-    print('Long answer data: ', num_of_long_answer)
-    print('Short answer data: ', num_of_short_answer)
-    return df_dataset, df_dataset_long
+        df_dataset.loc[num_of_answer] = [context] + [question] + [answer]
+        num_of_answer = num_of_answer + 1
+    return df_dataset
 
 
 if __name__ == "__main__":
@@ -40,32 +32,28 @@ if __name__ == "__main__":
 
     pd.set_option('display.max_colwidth', None)
     print('Loading df_train...')
-    df_train, df_train_long = load_squad_dataset(train_dataset)
+    df_train = load_squad_dataset(train_dataset)
     print('Loading df_validation...')
-    df_validation, df_validation_long = load_squad_dataset(valid_dataset)
+    df_validation = load_squad_dataset(valid_dataset)
 
     print('Shuffling DataFrame...')
     df_train = shuffle(df_train)
-    df_train_long = shuffle(df_train_long)
     df_validation = shuffle(df_validation)
-    df_validation_long = shuffle(df_validation_long)
 
-    print('df_train.shape')
-    print(df_train.shape)
-    print('df_validation.shape')
-    print(df_validation.shape)
-    print('df_train.head():')
-    print(df_train.head())
-    print('df_validation.head():')
-    print(df_validation.head())
+    # print('df_train.shape')
+    # print(df_train.shape)
+    # print('df_validation.shape')
+    # print(df_validation.shape)
+    # print('df_train.head():')
+    # print(df_train.head())
+    # print('df_validation.head():')
+    # print(df_validation.head())
 
     print('Saving dataset as csv...')
     dataset_save_path = 'datasets/'
     if not os.path.exists(dataset_save_path):
         os.makedirs(dataset_save_path)
     df_train.to_csv(dataset_save_path + 'squad_train.csv', index=False)
-    df_train_long.to_csv(dataset_save_path + 'squad_train_long.csv', index=False)
     df_validation.to_csv(dataset_save_path + 'squad_validation.csv', index=False)
-    df_validation_long.to_csv(dataset_save_path + 'squad_validation_long.csv', index=False)
     print('All done.')
 
